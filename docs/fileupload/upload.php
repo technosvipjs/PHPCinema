8152ea87d924b7d60d26911ea17654edab2481af
User <User@Lenovo>
2014-11-02 17:36:14 +0200

    File Upload

--- docs/fileupload/upload.php
+++ docs/fileupload/upload.php
@@ -1,31 +1,20 @@
 <?php
-if(isset($_REQUEST['submit']))
-{
-  $filename=  $_FILES["imgfile"]["name"];
-  if ((($_FILES["imgfile"]["type"] == "image/gif")|| ($_FILES["imgfile"]["type"] == "image/jpeg") || ($_FILES["imgfile"]["type"] == "image/png")  || ($_FILES["imgfile"]["type"] == "image/pjpeg")) && ($_FILES["imgfile"]["size"] < 200000))
-  {
-    if(file_exists($_FILES["imgfile"]["name"]))
-    {
-      echo "File name exists.";
-    }
-    else
-    {
-      move_uploaded_file($_FILES["imgfile"]["tmp_name"],"uploads/$filename");
-      echo "Upload Successful . <a href='uploads/$filename'>Click here</a> to view the uploaded image";
-    }
-  }
-  else
-  {
-    echo "invalid file.";
-  }
+
+if ( !empty( $_FILES ) ) {
+
+    $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
+    $uploadPath = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_FILES[ 'file' ][ 'name' ];
+
+    move_uploaded_file( $tempPath, $uploadPath );
+
+    $answer = array( 'answer' => 'File transfer completed' );
+    $json = json_encode( $answer );
+
+    echo $json;
+
+} else {
+
+    echo 'No files';
+
 }
-else
-{
-?>
-<form method="post" enctype="multipart/form-data">
-File name:<input type="file" name="imgfile"><br>
-<input type="submit" name="submit" value="upload">
-</form>
-<?php
-}
-?> 
+
